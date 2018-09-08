f1a34bcb96a84c66efb06f223909866b59f89f5e
tiagobruno <tiagobrunobfr@gmail.com>
2018-09-08 00:17:53 -0300

    ajuestes test tipo cardapio

--- tests/Feature/Enterprise/TypeMenuControllerTest.php
+++ tests/Feature/Enterprise/TypeMenuControllerTest.php
@@ -2,12 +2,18 @@
 
 namespace Tests\Feature\Enterprise;
 
+use App\Http\Controllers\Enterprise\TypeMenuController;
+use App\Services\Enterprise\TypeMenuService;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Http\Request;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-
 class TypeMenuControllerTest extends TestCase
 {
+
+    use DatabaseMigrations;
+
     /**
      * A basic test example.
      *
@@ -21,6 +27,18 @@ class TypeMenuControllerTest extends TestCase
 
     public function test_a_user_can_create_type_menu()
     {
+
+        $data = [
+            'name' => 'Teste 1'
+        ];
+
+       $response = $this->post(route('enterprise.typemenu.create.post', $data));
+
+        //$response->assertRedirect(route('enterprise.typemenu.list.get'));
+
+        $this->assertDatabaseHas('type_menus', [
+            'name' => 'Teste 1'
+        ]);
 
     }
 
